79616cbaa4181430c5791e6015310c7e0461de4a
Sylphias <sylpharyon@gmail.com>
2015-11-28 11:38:06 +0800

    Added File writing capabilities to the code

--- RangeTest_V2.ino
+++ RangeTest_V2.ino
@@ -1,9 +1,11 @@
 #include <XBee.h>
 #include <SoftwareSerial.h>
-//#include <LiquidCrystal.h>
- 
-//LiquidCrystal lcd(12,11,5,4,3,2);
- 
+#include <SPI.h>
+#include <SD.h>
+/*
+This file is used to do range testing, it sends out packets to another xbee and then receives the packet from the xbee 
+*/
+
 // XBee's DOUT (TX) is connected to pin 8 (Arduino's Software RX)
 // XBee's DIN (RX) is connected to pin 9 (Arduino's Software TX)
 SoftwareSerial serial1(0, 1); // RX, TX
@@ -12,7 +14,9 @@ XBee xbee=XBee();
 XBeeResponse response = XBeeResponse();
 Rx16Response rx16 = Rx16Response();
 Rx64Response rx64 = Rx64Response();
-uint8_t payload[] ={'T','e','s','t'}; 
+
+//Simulate actual packet length
+uint8_t payload[] ={'T','e','s','t','i','n','g',' ','P','a','l','o','a','d'}; 
 Tx16Request tx = Tx16Request(0x0000, payload, sizeof(payload));
 uint8_t option = 0;
 uint8_t data = 0;
@@ -23,38 +27,47 @@ void setup()
   Serial.begin(9600);
   serial1.begin(9600);
   xbee.setSerial(Serial);
-//  lcd.begin(16,2);
-//  lcd.clear();
 }
  
 void loop() 
 {
-//  xbee.readPacket(100);
+  xbee.readPacket(100);
   xbee.send(tx);
- if (xbee.getResponse().isAvailable())
- {
-   Serial.println("Getting Signal Strength: ");
-   
-   if(xbee.getResponse().getApiId() == RX_64_RESPONSE || xbee.getResponse().getApiId() == RX_16_RESPONSE)
-   { 
-     if (xbee.getResponse().getApiId() == RX_16_RESPONSE) 
-     {
+  if (xbee.getResponse().isAvailable())
+  {
+    Serial.println("Getting Signal Strength: ");
+
+    if(xbee.getResponse().getApiId() == RX_64_RESPONSE || xbee.getResponse().getApiId() == RX_16_RESPONSE)
+    { 
+      if (xbee.getResponse().getApiId() == RX_16_RESPONSE) 
+      {
        xbee.getResponse().getRx16Response(rx16);
        rssi = rx16.getRssi();
        Serial.println(rssi);
-       Serial.println(ab);
-     } 
-     else 
-     {
+       write_rssi_to_file(rssi);
+      } 
+      else 
+      {
        Serial.println("64");
        xbee.getResponse().getRx64Response(rx64);
        rssi = rx64.getRssi();
-//        lcd.clear();
-//        lcd.print(rssi);
        Serial.println(rssi);
-//        xbee.send(rssi);
-     }
-   }
- }
+      }
+    }
+  }
 delay(1000);
+}
+
+void write_rssi_to_file(uint8_t signal_str){
+  myFile = SD.open("rssi_values.txt", FILE_WRITE);
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.println("Signal Strength");
+    myFile.println(signal_str);
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
