c955ecfb4f3b431f9ad097a536808be476b2a8a4
Sylphias <sylpharyon@gmail.com>
2015-11-30 13:42:01 +0800

    Corrected a silly mistake

--- RangeTest_V2.ino
+++ RangeTest_V2.ino
@@ -9,7 +9,7 @@ This file is used to do range testing, it sends out packets to another xbee and 
 // XBee's DOUT (TX) is connected to pin 8 (Arduino's Software RX)
 // XBee's DIN (RX) is connected to pin 9 (Arduino's Software TX)
 SoftwareSerial serial1(0, 1); // RX, TX
-ledPin = 4;
+int ledPin = 4;
 XBee xbee=XBee();
 XBeeResponse response = XBeeResponse();
 Rx16Response rx16 = Rx16Response();
